c4900ba8e90876ccb4cb1a51d7f889cd70a0963f
rohit <rohit.apte89@gmail.com>
2020-07-10 16:32:35 +0200

    changes added for spring security jpa

---
+++ src/main/java/com/rohit/learning/springsecurityjpa/UserRepository.java
@@ -0,0 +1,11 @@
+package com.rohit.learning.springsecurityjpa;
+
+import com.rohit.learning.springsecurityjpa.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByUsername(String username);
+}
---
+++ src/main/java/com/rohit/learning/springsecurityjpa/models/User.java
@@ -0,0 +1,55 @@
+package com.rohit.learning.springsecurityjpa.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String username;
+    private String password;
+    private String enabled;
+    private String roles;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return username;
+    }
+
+    public void setUserName(String userName) {
+        this.username = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(String enabled) {
+        this.enabled = enabled;
+    }
+
+    public String getRoles() {
+        return roles;
+    }
+
+    public void setRoles(String roles) {
+        this.roles = roles;
+    }
+}
